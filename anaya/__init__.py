92eb54aebccc247d68fc1dc36a38bfed0912fb75
Shreyas Waghnode <shreyasw496@gmail.com>
2024-07-04 08:18:03 +0530

    chore: Add Anaya to the repository

---
+++ anaya/__init__.py
@@ -0,0 +1,1 @@
+from anaya import Anaya
---
+++ main.py
@@ -0,0 +1,9 @@
+from anaya import Anaya
+
+if __name__ == "__main__":
+    anaya = Anaya(
+            title="Anaya🔥📑",
+            initial_message="Hello! I am **Anaya🔥**. How can I help you today?",
+            model="llama3:8b"
+        )
+    anaya.run()
