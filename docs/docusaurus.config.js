74e228787f3650635f6cbb8594171890cf462ca0
Shreyas Waghnode <shreyasw496@gmail.com>
2024-07-04 08:38:46 +0530

    chore: Update baseUrl in docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://swax10.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/anaya/v0.1/",
+  baseUrl: "/anaya/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/v0.1/docs/introduction">
+            to="/docs/introduction">
             Anaya🔥📑 Docs - 5min ⏱️
           </Link>
         </div>
